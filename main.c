dfd0f3a3a34140d51905191b66d4ba9b1be9a3d7
Ca7Ac1 <Ca7Ac1@gmail.com>
2021-11-16 20:22:42 -0500

    Completed work

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <time.h>
+
+int main()
+{
+    printf("Parent starting!\n");
+
+    int f = fork();
+
+    if (f)
+    {
+        
+        f = fork();
+    }
+
+
+    if (f)
+    {
+        int status;
+        wait(&status);
+
+        printf("Child asleep for %d seconds\n", WEXITSTATUS(status));
+        printf("Parent finished!\n");
+    }
+    else
+    {
+        srand(getpid());
+
+        int seconds = rand() % 4 + 2;
+        printf("Child's PID: %d\n", getpid());
+
+        sleep(seconds);
+        printf("Child %d Finished\n", getpid());
+        // printf("PID: %d Seconds:%d\n", getpid(), seconds);
+
+        return seconds;
+    }
+
+    return 0;
+}
